64a8513b2aa97408ffb99bdac957402dcf8e3db4
Carl Lee <ckdwkdl91@gmail.com>
2026-02-03 09:13:23 +0900

    ✨ Feat: add row and column

--- app/src/main/java/com/example/carlexample/MainActivity.kt
+++ app/src/main/java/com/example/carlexample/MainActivity.kt
@@ -4,44 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.carlexample.ui.theme.CarlexampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            CarlexampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MessageCard(Message("World","Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body:String)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) {
+    Row{
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "더미 사진입니다.",
+        )
+
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CarlexampleTheme {
-        Greeting("Android")
-    }
+fun MessageCardPreview() {
+    MessageCard(Message("World", "Jetpack Compose"))
 }
