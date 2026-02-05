5180261e0dc00029ffa58112e0bca0c1047cb81b
Carl Lee <ckdwkdl91@gmail.com>
2026-02-05 14:55:16 +0900

    📦 Build: add dependencies of coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.ads.mobile.sdk)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
