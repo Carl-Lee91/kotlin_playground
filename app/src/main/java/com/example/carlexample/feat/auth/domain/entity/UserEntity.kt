0e17b44fe109bf9ff588ff6c00fb47e59324e846
Carl Lee <ckdwkdl91@gmail.com>
2026-02-05 11:16:14 +0900

    ✨ Feat: add domain layer of auth

---
+++ app/src/main/java/com/example/carlexample/feat/auth/domain/entity/UserEntity.kt
@@ -0,0 +1,6 @@
+package com.example.carlexample.feat.auth.domain.entity
+
+data class UserEntity(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/example/carlexample/feat/auth/domain/repository/LoginRepository.kt
@@ -0,0 +1,8 @@
+package com.example.carlexample.feat.auth.domain.repository
+
+import com.example.carlexample.core.common.Result
+import com.example.carlexample.feat.auth.domain.entity.UserEntity
+
+interface LoginRepository {
+    fun makeLoginRequest(jsonBody: String): Result<UserEntity>
+}
